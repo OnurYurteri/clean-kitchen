f14bbf62349d389741605bd95a18abf5064e12b5
Onur Yurteri <onur.yurteri@inomera.com>
2021-12-21 14:03:01 +0300

    Initial commit 🚀

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import MainPage from './setup/main-page/index'
+
+ReactDOM.render(
+  <StrictMode>
+    <MainPage></MainPage>
+  </StrictMode>,
+  document.getElementById('root')
+)
