3999065eb3d8ebbff88037b650dace05ccf0f3fa
Onur Yurteri <onur.yurteri@inomera.com>
2021-12-21 14:08:51 +0300

    Bind services to react component states

---
+++ src/setup/main-page/index.tsx
@@ -0,0 +1,43 @@
+import './MainPage.css'
+import DatePicker from '../../component/date-picker/'
+import { useState } from 'react'
+import SlotSelector from '../../component/slot-selector'
+import { getAvailablityForDate } from '../../service/MainPageService'
+import Availability from '../../types/Availability'
+import { dateFromStr, nowDateStr } from '../../util'
+
+function MainPage() {
+  const [date, setDate] = useState(nowDateStr)
+  const [availability, setAvailability] = useState<Availability[]>(
+    (): Availability[] => {
+      return getAvailablityForDate(dateFromStr(date))
+    }
+  )
+
+  const refreshAvailability = (date: Date) => {
+    const res = getAvailablityForDate(date)
+    setAvailability(res)
+  }
+
+  const changed = (value: string) => {
+    setDate(value)
+    refreshAvailability(dateFromStr(value))
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DatePicker
+          style={{ height: '2.5rem', margin: '15px' }}
+          onChange={changed}
+          value={date}
+        ></DatePicker>
+        {date ? (
+          <SlotSelector availability={availability}></SlotSelector>
+        ) : null}
+      </header>
+    </div>
+  )
+}
+
+export default MainPage
